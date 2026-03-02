57bdb627db431e401cf3930ab79eb668d0fba861
Heng Yang <yangheng2021@gmail.com>
2026-03-03 00:22:49 +0800

    auto-commit

--- packages/opencorvus/test/tool/read.test.ts
+++ packages/opencorvus/test/tool/read.test.ts
@@ -163,7 +163,7 @@ describe("tool.read env file permissions", () => {
     ["environment.ts", false],
   ]
 
-  describe.each(["build", "plan"])("agent=%s", (agentName) => {
+  describe.each(["build", "general"])("agent=%s", (agentName) => {
     test.each(cases)("%s asks=%s", async (filename, shouldAsk) => {
       await using tmp = await tmpdir({
         init: (dir) => Bun.write(path.join(dir, filename), "content"),
