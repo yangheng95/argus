59055ec8d969a9ec478969d2c424cf5bdb244fe9
Heng Yang <yangheng2021@gmail.com>
2026-03-05 00:33:44 +0800

    auto-commit

--- packages/sdk/js/src/v2/gen/client.gen.ts
+++ packages/sdk/js/src/v2/gen/client.gen.ts
@@ -15,4 +15,4 @@ export type CreateClientConfig<T extends ClientOptions = ClientOptions2> = (
   override?: Config<ClientOptions & T>,
 ) => Config<Required<ClientOptions> & T>
 
-export const client = createClient(createConfig<ClientOptions2>({ baseUrl: "http://localhost:4096" }))
+export const client = createClient(createConfig<ClientOptions2>({ baseUrl: "http://localhost:7878" }))
