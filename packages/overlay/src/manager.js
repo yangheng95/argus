4316a8e2da573526375b4e27c4125d08d8c8d85f
Heng Yang <yangheng2021@gmail.com>
2026-03-04 18:36:28 +0800

    auto-commit

--- packages/overlay/src/manager.js
+++ packages/overlay/src/manager.js
@@ -97,7 +97,7 @@ function renderCommandPreview() {
   state.runArgs = runArgs
   if (els.serveFixedInput) els.serveFixedInput.value = serveArgs.join(" ")
   if (els.runFixedInput) els.runFixedInput.value = `${runArgs.join(" ")} "<prompt>"`
-  if (els.servePreviewInput) els.servePreviewInput.value = cmd(command, serveArgs)
+  if (els.servePreviewInput) els.servePreviewInput.value = cmdLine(command, serveArgs)
   if (els.runPreviewInput) els.runPreviewInput.value = `${cmd(command, runArgs)} "<prompt>"`
 }
 
