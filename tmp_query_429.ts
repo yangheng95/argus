cee30e5696d20cef7e57172bab2d06d5af146518
Heng Yang <yangheng2021@gmail.com>
2026-04-26 01:44:06 +0800

    dsw-33987 chore: remove accidental tmp_query_429.ts

--- tmp_query_429.ts
+++
@@ -1,8 +0,0 @@
-import { Database } from "bun:sqlite";
-
-const db = new Database("C:/Users/hengu/.local/share/opencorvus/opencorvus.db", { readonly: true });
-const TASK = "tsk_dc59a62d7001wJppaWrxgEkSj6";
-
-const tables = db.query(`SELECT name FROM sqlite_master WHERE type='table' ORDER BY name`).all();
-console.log("TABLES:");
-for (const t of tables as any[]) console.log("  -", t.name);
